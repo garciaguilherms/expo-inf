c772df07635adb26be156fdab0f984478cec7930
Guilherme <gmgarcia@inf.ufsm.br>
2024-02-20 00:39:06 -0300

    padroniza tamanho das imagens mostradas e estiliza descrição

--- resources/js/Components/TipTap.vue
+++ resources/js/Components/TipTap.vue
@@ -5,6 +5,7 @@
             class="buttons flex items-center flex-wrap gap-x-4 border-t border-l border-r border-gray-300 p-2 rounded-md"
         >
             <button
+                type="button"
                 @click="editor.chain().focus().toggleBold().run()"
                 class="rounded-md p-1"
                 :disabled="!editor.can().chain().focus().toggleBold().run()"
@@ -13,6 +14,7 @@
                 bold
             </button>
             <button
+                type="button"
                 @click="editor.chain().focus().toggleItalic().run()"
                 class="rounded-md p-1"
                 :disabled="!editor.can().chain().focus().toggleItalic().run()"
@@ -21,6 +23,7 @@
                 italic
             </button>
             <button
+                type="button"
                 @click="editor.chain().focus().toggleCode().run()"
                 class="rounded-md p-1"
                 :disabled="!editor.can().chain().focus().toggleCode().run()"
@@ -29,6 +32,7 @@
                 code
             </button>
             <button
+                type="button"
                 @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
                 class="rounded-md p-1"
                 :class="{
@@ -38,6 +42,7 @@
                 h1
             </button>
             <button
+                type="button"
                 @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
                 class="rounded-md p-1"
                 :class="{
@@ -47,6 +52,7 @@
                 h2
             </button>
             <button
+                type="button"
                 @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
                 class="rounded-md p-1"
                 :class="{
@@ -56,6 +62,7 @@
                 h3
             </button>
             <button
+                type="button"
                 @click="editor.chain().focus().toggleBulletList().run()"
                 class="rounded-md p-1"
                 :class="{ 'bg-gray-400 rounded-md p-1': editor.isActive('bulletList') }"
@@ -63,6 +70,7 @@
                 bullet list
             </button>
             <button
+                type="button"
                 @click="editor.chain().focus().toggleOrderedList().run()"
                 class="rounded-md p-1"
                 :class="{ 'bg-gray-400 rounded-md p-1': editor.isActive('orderedList') }"
--- resources/js/Pages/Projects/Project.vue
+++ resources/js/Pages/Projects/Project.vue
@@ -1,5 +1,5 @@
 <template>
-    <Head title="Dashboard" />
+    <Head title="Dashboard"/>
     <AuthenticatedLayout>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -8,17 +8,15 @@
                         <div class="p-6 text-gray-900 flex justify-end flex-col">
                             <div class="image-ranking-box">
                                 <img :src="project.image" class="project-image" alt="Imagem do projeto" />
-                                <div>
+                                <div class="rating-box">
                                     <div class="average-rating">
                                         <font-awesome-icon :icon="['fas', 'star']" size="2xl" style="color: #f2c445" />
-                                        {{ project.average_stars }}
+                                        <span class="rating-text">{{ project.average_stars }}</span>
                                     </div>
                                 </div>
                             </div>
                             <h2 class="project-title">{{ project.title }}</h2>
-                            <p class="project-description">
-                                {{ project.description }}
-                            </p>
+                            <div class="my-8 prose max-w-none" style="word-wrap: break-word" v-html="project.description" ></div>
                             <p class="project-artist">Autores:</p>
                             <div v-for="author in project.authors">
                                 <p class="project-artist">{{ author.name }}</p>
@@ -27,8 +25,8 @@
                         <div class="project-comment-box">
                             <h2 class="text-black font-bold">Comentários</h2>
                             <div class="comment-box" v-for="comment in project.comments">
-                                <p>{{ comment.user.name }}</p>
-                                <p>{{ comment.text }}</p>
+                                <p class="comment-user">{{ comment.user.name }}</p>
+                                <p class="comment-text">{{ comment.text }}</p>
                             </div>
                         </div>
                     </div>
@@ -52,33 +50,23 @@ export default {
     }
 }
 </script>
-
 <style scoped>
 .project-image {
-    width: 50%;
-    height: 50%;
-    border-radius: 8px;
     object-fit: cover;
-    margin-bottom: 10px;
-    margin-top: 10px;
+    width: 100%;
+    height: 100%;
 }
-
 .project-title {
-    font-size: 18px;
+    display: flex;
+    justify-content: center;
+    margin: 10px;
+    font-size: 25px;
     font-weight: bold;
 }
-
-.project-description {
-    font-size: 14px;
-    font-weight: bold;
-    color: #888;
-}
-
 .project-artist {
     font-size: 12px;
     color: #888;
 }
-
 .project-comment-box {
     background-color: #f8f9fa;
     border: 1px solid #ced4da;
@@ -86,13 +74,11 @@ export default {
     padding: 1rem;
     margin-bottom: 1rem;
 }
-
 .project-comment-box h2 {
     color: #000;
     font-weight: bold;
     margin-bottom: 0.5rem;
 }
-
 .comment-box {
     background-color: #fff;
     border: 1px solid #ced4da;
@@ -100,17 +86,16 @@ export default {
     padding: 0.5rem;
     margin-bottom: 0.5rem;
 }
-
 .comment-box p {
     margin-bottom: 0.25rem;
 }
-
 .image-ranking-box {
     display: flex;
-    align-items: center;
-    justify-content: space-between;
+    justify-content: center;
+    max-width: 100%;
+    height: 500px;
+    position: relative;
 }
-
 .average-rating {
     font-size: 20px;
     color: #888;
@@ -118,5 +103,26 @@ export default {
     overflow: hidden;
     text-overflow: ellipsis;
     margin-right: 10px;
+}
+.rating-box {
+    position: absolute;
+    bottom: 0px;
+    right: 0px;
+    background-color: #888;
+    padding: 8px;
+    border-radius: 4px 0px 0px 0px;
+}
+.rating-text {
+    margin-left: 5px;
+    color: white;
+}
+.comment-user {
+    font-weight: bold;
+}
+.comment-text {
+    margin-left: 10px;
+}
+.prose {
+    word-wrap: break-word;
 }
 </style>
