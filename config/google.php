dfe3d4877993cc9f7d9b561c4dd30957722606f3
Guilherme <gmgarcia@inf.ufsm.br>
2024-07-07 19:04:12 -0300

    google.php

--- config/google.php
+++ config/google.php
@@ -55,7 +55,19 @@ return [
          * Path to service account json file. You can also pass the credentials as an array
          * instead of a file path.
          */
-        'file' => env('GOOGLE_SERVICE_ACCOUNT_FILE', storage_path('expoinf-12c91600690f.json')),
+        'file' => [
+            "type" => "service_account",
+            "project_id" => "expoinf",
+            "private_key_id" => "12c91600690f9bf17d7e07f418593b1f3b90a65c",
+            "private_key" => "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCx2X4WhQiQ2Tcq\ngEBo+lgy69N+1xOA7hlZ212D78GMpk5zoXU54gaXxursxSiBzYkFgIB44Fz4MvHt\nikQquI9UGO2i3eUN99gfz2jSkMNiMDP/sDcRYCgpcTY31p0a/hHWxQjbPhBqyms/\njaF3GKRFlxKhaJWi4Oxt0eWN5PmD8cs/pkW16kxtxRbrytG0yDWLLGBBrWT+o1gI\n0VjIRuWVz+kltQjrBPI/M+MgrzlGA/rMmCN9JIuhwvx943vOc/qr13GoU4fORk2T\ncoClrUzpYG4y60nRi1tXdCFqX2H15HsKXD33a7Gf/Ty3fMam6GmPfUkApeL9BRKa\nHrHIErZJAgMBAAECggEABvOnzMYhXXXAz6ZwzhcCBqcxQ/cbe1nN5R/7XevBB/1y\nBIL6RRt8szxVOhEjVL6QWsYTpNXtSw4vvB+gVj9S/+rTssmZDq9CSWJcYtsB03cr\noyoTyaK1ndgVEitEsRWfDs3Hxg0dIRV9ih/gFhALz29jglUR60p+3LSR3uE6mmxd\njfH4uz1LwWc+Pc4143OdvuxPSewiHyOCt03PmHR7jhIu4FFZOwZDSv9zhCfcHgT+\nOfAeduaMV4Gv2apFJaHkYDkpY2hIuzU2JWGinVeCwYTzM/Qlm8aBBgE5Frj2z4k5\n6WtWSGTz0cqpvhjzbeTqg/26PAIl8u5rid1Aj84NYQKBgQDfzozZVGgu/f6w9JWi\nx2fuCBt2+l5aLihkRGXZ3tkjCWlSy3i8K2QuJy1tOKMUE49n+jtfjh+cEwmkJYW4\nwgnNaSoeoknHexcKrQe6WquYa6l3bqdSaq0zHL728hEIqT+Hl0qGU+tc2y4w9z8f\n2Lj7UsBQxz0e4ze2hK8eYqKuoQKBgQDLbp1yX1lAR+ws37c6JS0G8mx3aibRoBY7\n5FAGLaVwP3yQv2/sStLBS23T4x8XxbRTxUyOiLUF7+XgItLCzeLKk8gbl1cuez0V\nWJ7/m2cpMHmNs1iROT7N7z3hyVFKFJLrEMbOMkHsFtfg1SuGBI1NT2fTISy12jML\ne+0GQnyuqQKBgQCqFSFrJD9MvxAoUKfLHfl4psagg6Liq8Ncd7JjgXzIqKVXG3R0\npBEtFe7q2yjr3/WS0lL+ylZxWPJxwWY/Q48JgMtWnoStSXj4TwjZcYCIhAE0123n\nL1TSzBzANLQHPgAevjYPoYTze0o+QhfBzwS9UmT+vntPluPHHEytLc5uIQKBgQCp\nW814Cikjer0EHrtLjOePgQML5zOqELxx4AE/I4E3kaMCXl2vOht7EHC+lbas88rM\nctTpVKZf6vhVQKh1xTGptJ5DWhsadz7DJqQ36AhaIiEP9t8lB6NhjmrWLeOe6MHn\nMHBR+UUoZ1wRsWYqjF+WnSSygEvYmW8lgGYzb+adAQKBgQDYjX3MYcfHV+7DncAn\nJB5OLxsegdPT7yyRIDcEu15idH8/23pTNPmOpSVYTcvQ8FXvPBQ4/SYkaeSsSmdu\n+w6LLJGtPucxdbuy8eWQeuStcgzhd7EC+UX1Hi5WGtzWvIhbKpAgse9QoAM5dATw\nhWG2UQyZCa0F0+NgwYltxk7kqg==\n-----END PRIVATE KEY-----\n",
+            "client_email" => "google-sheet-api@expoinf.iam.gserviceaccount.com",
+            "client_id" => "116288463541989575513",
+            "auth_uri" => "https://accounts.google.com/o/oauth2/auth",
+            "token_uri" => "https://oauth2.googleapis.com/token",
+            "auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
+            "client_x509_cert_url" => "https://www.googleapis.com/robot/v1/metadata/x509/google-sheet-api%40expoinf.iam.gserviceaccount.com",
+            "universe_domain" => "googleapis.com"
+        ],
     ],
 
     /*
